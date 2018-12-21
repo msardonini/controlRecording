d06e111378efcc201ab296b4f1e793b928add637
Mike <msardonini@gmail.com>
2018-12-21 02:36:15 +0000

    fixed the install directory of serrvice files

--- install/start_remoteSender_on_boot.sh
+++ install/start_remoteSender_on_boot.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 
-sudo cp remoteSender.service /lib/systemd/system/
-sudo cp rfcomm.service /lib/systemd/system/
+sudo cp remoteSender.service /etc/systemd/system/
+sudo cp rfcomm.service /etc/systemd/system/
 
 sudo systemctl daemon-reload
 sudo systemctl enable remoteSender
